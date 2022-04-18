91183dc1659176788622be5608397afac6ebd18b
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2022-04-19 03:35:35 +0530

    Basic UI Completed

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import UserResults from "../components/Users/UserResults";
+function Home() {
+	return (
+		<>
+			{/* Search Component  */}
+			<UserResults />
+		</>
+	);
+}
+
+export default Home;
