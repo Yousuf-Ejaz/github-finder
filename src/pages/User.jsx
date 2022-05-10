0572dc054b504102910734a2a839a03c06a14901
Yousuf Ejaz Ahmad <ejazahmadyousuf2@gmail.com>
2022-05-10 16:01:02 +0530

    Added Axios

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,7 +5,7 @@ import Spinner from "../components/Layout/Spinner";
 import { useParams } from "react-router-dom";
 import GithubContext from "../context/github/GithubContext";
 import RepoList from "../components/repos/RepoList";
-import { getUser, getUserRepos } from "../context/github/GithubActions";
+import { getUserAndRepos } from "../context/github/GithubActions";
 
 function User() {
 	const { user, loading, repos, dispatch } = useContext(GithubContext);
@@ -15,15 +15,14 @@ function User() {
 			type: "SET_LOADING",
 		});
 		const getUserData = async () => {
-			const userData = await getUser(params.login);
-			dispatch({ type: "GET_USER", payload: userData });
+			const userData = await getUserAndRepos(params.login);
+			dispatch({ type: "GET_USER_AND_REPOS", payload: userData });
+			
 
-			const repoData = await getUserRepos(params.login);
-			dispatch({ type: "GET_REPOS", payload: repoData });
 		};
 
 		getUserData();
-	}, []);
+	}, [dispatch, params.login]);
 	const {
 		name,
 		type,
